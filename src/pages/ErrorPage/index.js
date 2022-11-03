49c14e7174c6b32638dc23021056dafb11bb76e2
Elif Çiftçi <eliffbat@gmail.com>
2022-11-04 00:01:34 +0300

    error page was designed

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "./style.module.css";
 
 //Ant-design
 import "antd/dist/antd.css";
-import { Space, Typography } from "antd";
+import { Button, Space, Typography } from "antd";
 const { Title } = Typography;
 
 const ErrorPage = () => {
   return (
-    <Space size="large" align="center">
-      <Title level={1}>Error</Title>
-      <Space>
+    <Space className={style.errorPageContainer} size="large" align="center">
+      <Title level={1} className={style.errorPageTitle}>
+        Error
+      </Title>
+      <Space size={30} className={style.errorPageInnerContainer}>
         {/* Chuck Norris Gif */}
-        <img
-          src="https://media.tenor.com/GdF28omU-1YAAAAC/chuck-norris-kick-action.gif"
-          style={{
-            width: "100px",
-            height: "130px",
-            backgroundColor: "unset",
-          }}
-          alt="Chuck Norris Kick Action GIF - Chuck Norris Kick Action Walker Texas Ranger GIFs"
-        />
-        <p>The page you were looking for was not found</p>
-      </Space>
+        <Space>
+          <img
+            src="https://media.tenor.com/GdF28omU-1YAAAAC/chuck-norris-kick-action.gif"
+            style={{
+              width: "100px",
+              height: "130px",
+              borderRadius: "20px",
+            }}
+            alt="Chuck Norris Kick Action GIF - Chuck Norris Kick Action Walker Texas Ranger GIFs"
+          />
+          <p className={style.errorPageText}>
+            The page you are looking for was not found or you are not logged in!{" "}
+          </p>
+        </Space>
 
-      <Space align="center">
-        <Link to="/">Main</Link>
+        <Link className={style.errorPageLink} to="/">
+          Main
+        </Link>
       </Space>
     </Space>
   );
